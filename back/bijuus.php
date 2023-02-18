990636bab0e4948c053953516595f672cf2fd1f7
EmanueDevid <emanueldavid150@gmail.com>
2023-02-17 23:56:53 -0300

    responsividade tela inicial

---
+++ back/bijuus.php
@@ -0,0 +1,13 @@
+<?php
+
+class Bijuu
+{
+    private $nome = null;
+    private $animal = null;
+    private $qtd_caudas = 0;
+    private $jinchuuriki_id = null;
+    private $descricao = null;
+    private $aldeia = null;
+    private $status = null;
+    private $imagem = null;
+}
