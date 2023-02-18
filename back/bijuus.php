305592c7d5d7d1d81bf07cf63aaed6a9e902a9c4
EmanueDevid <emanueldavid150@gmail.com>
2023-02-18 00:12:32 -0300

    cria getters e setters de Bijuus

--- back/bijuus.php
+++ back/bijuus.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once("Conexao.php");
 class Bijuu
 {
     private $nome = null;
@@ -9,5 +10,94 @@ class Bijuu
     private $descricao = null;
     private $aldeia = null;
     private $status = null;
-    private $imagem = null;
+    private $imagem_src = null;
+
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+        return $this;
+    }
+
+    public function getAnimal()
+    {
+        return $this->animal;
+    }
+
+    public function setAnimal($animal)
+    {
+        $this->animal = $animal;
+        return $this;
+    }
+
+    public function getQuantidade_caudas()
+    {
+        return $this->qtd_caudas;
+    }
+
+    public function setQuantidade_caudas($quantidade_caudas)
+    {
+        $this->qtd_caudas = $quantidade_caudas;
+        return $this;
+    }
+
+    public function getJinchuuriki()
+    {
+        return $this->jinchuuriki_id;
+    }
+
+    public function setJinchuuriki($jinchuuriki_id)
+    {
+        $this->jinchuuriki_id = $jinchuuriki_id;
+        return $this;
+    }
+
+    public function getDescricao()
+    {
+        return $this->descricao;
+    }
+
+    public function setDescricao($descricao)
+    {
+        $this->descricao = $descricao;
+        return $this;
+    }
+
+    public function getAldeia()
+    {
+        return $this->aldeia;
+    }
+
+    public function setAldeia($aldeia)
+    {
+        $this->aldeia = $aldeia;
+        return $this;
+    }
+
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    public function setStatus($status)
+    {
+        $this->status = $status;
+        return $this;
+    }
+
+    function getImagem()
+    {
+        return $this->imagem_src;
+    }
+
+    function setImagem($imagem_src)
+    {
+        $this->imagem_src = $imagem_src;
+        return $this;
+    }
+    
 }
